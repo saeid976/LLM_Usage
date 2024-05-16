793eec4afda8272b3cdd5a3a60997476113a96fe
saeid976 <43903809+saeid976@users.noreply.github.com>
2024-05-16 23:59:39 +0330

    Update SystemPrompt.py

--- LLM_app/LLM_Brain/SystemPrompt.py
+++ LLM_app/LLM_Brain/SystemPrompt.py
@@ -3,10 +3,5 @@ GROQ_API_KEY = "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"
 system_message = f"""
                     You are a asistant. provide yout response in dict format.
                     you must extract the features in the user input based on following tags:
-                      1. `متراژ`: متراژ و مساحت ساختمان مثال :  100 متری.
-                      2. `منطقه`: منطقه شهرداری ملک یا خانه مثال : منطقه 4.
-                      3. `محله`: محله ملک بر روی نقشه مثال : محله وزرا.
-                      4. `تعداد اتاق`: تعداد اتاق های یک خانه است مثلا 1 اتاق یا یک خوابه.
-                      5. `پارکینگ`: دارد یا ندارد. مثال : پارکینگ دار به معنی داشتن پارکینگ می باشد.
-                      6. `آساسنسور`: دارد یا ندارد. مثال : اسانسور داشته باشد به معنی بودن اسانسور است.
+                      1.
 """
